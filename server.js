d3b96777825d6dfc2316024ee858fa0ce91d64ed
tariktabakovic <tariktab97@gmail.com>
2020-03-06 18:48:32 -0500

    implements pg.pool for connection pooling

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const pg = require('pg');
 const PORT = 3000; 
 const app = express();
 
+let pool = new pg.Pool ({
+    port: 5432,
+    password: 'tarik',
+    database: 'capstone-database',
+    max: 10
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
