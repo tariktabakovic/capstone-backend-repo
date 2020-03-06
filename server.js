dc573ff6aa301f45c1d7742ce326976e58e68d86
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2020-03-06 18:08:59 -0500

    uses morgan; HTTP request logger middleware

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(morgan('dev'));
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
