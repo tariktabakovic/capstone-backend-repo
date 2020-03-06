cb370904423a197046758f3c6318fecb9e78ad53
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2020-03-06 18:00:43 -0500

    applies middleware bodyParser

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
