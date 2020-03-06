e6a234275c8be62a92ae7254954526d4df1e9fb9
tariktabakovic <tariktab97@gmail.com>
2020-03-06 18:17:39 -0500

    implements app.listen to PORT

--- server.js
+++ server.js
@@ -15,3 +15,5 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.listen(PORT, () => console.log('listening on port' + PORT)); 
