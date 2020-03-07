f64e640b280c03a338b614c88bfec38ec78c3606
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2020-03-06 19:25:01 -0500

    changes user in pg.pool & connects the pool to database

--- server.js
+++ server.js
@@ -6,11 +6,31 @@ const PORT = 3000;
 const app = express();
 
 let pool = new pg.Pool ({
-    port: 5432,
-    password: 'tarik',
+    user: 'tarik',
     database: 'capstone-database',
+    password: 'tarik',
+    host: 'localhost',
+    port: 5432,
     max: 10
 });
+
+pool.connect((err, db, done)=> {
+    if(err){
+        return console.log(err);
+    } 
+    else {
+        db.query('SELECT * from users', (err, table) => {
+            done();
+            if(err){
+                return console.log(err)
+            }
+            else{
+                console.log(table.rows)
+            }
+        })
+    }
+})
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
