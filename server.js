bfe40442c37a0dcbee31e2e9f32b684dd6d52f61
tariktabakovic <tariktab97@gmail.com>
2020-03-08 22:18:07 -0400

    require DB, and connects it with mongoose

--- server.js
+++ server.js
@@ -34,17 +34,18 @@ pool.connect((err, db, done)=> {
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(morgan('dev'));
+// DB Config
+const db = require('./config/keys').mongoURI;
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+// 
+mongoose
+    .connect(db)
+    .then(()=> console.log('Mongo DB connected'))
+    .catch(err=> console.log(err))
+    
+const port=process.env.PORT || 5000;
 
-app.post('/api/new-account', function(request, respnse){
-    console.log(request.body);
-});
-app.listen(PORT, () => console.log('listening on port' + PORT)); 
+
+app.listen(port, ()=> console.log(`Server started on port ${port}`))
+
