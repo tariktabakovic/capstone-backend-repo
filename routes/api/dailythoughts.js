e2105bc4ee1a3931c8a9c17b656f6bd573d218ae
tariktabakovic <tariktab97@gmail.com>
2020-03-09 01:33:12 -0400

    creates router for dailythoughts api

--- routes/api/dailythoughts.js
+++ routes/api/dailythoughts.js
@@ -4,4 +4,14 @@ const router = express.Router();
 // DailyThought model
 const DailyThought = require('../../models/DailyThoughts');
 
+// @route GET api/dailythoughts
+// @desc Get All dailythoughts
+// @access Public
+
+router.get('/', (req, res)=> {
+    DailyThought.find()
+        .sort({date: -1})
+        .then(DailyThoughts => res.json(DailyThoughts))
+});
+
 module.exports = router;
