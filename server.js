a503bdbeb5bdc8b2bfb711994fa7fdf272c941c4
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2020-03-09 01:34:05 -0400

    fix typo in server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/api/dailythoughts', dailythoughts);
 
 const port=process.env.PORT || 5000;
 
-app.use('/api/users', require('./routes/api/users'));
+// app.use('/api/users', require('./routes/api/users'));
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
 
