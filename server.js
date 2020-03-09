60a9092d09385f8da89970987e4baff0acfb0470
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2020-03-08 22:57:01 -0400

    app.use for bodyparser middleware

--- server.js
+++ server.js
@@ -1,36 +1,8 @@
 const express = require('express');
 const mongoose = require('mogoose');
-const bodyParser = require('body-parser');
-const pg = require('pg');
 const app = express();
 
-let pool = new pg.Pool ({
-    user: 'tarik',
-    database: 'capstone-database',
-    password: 'tarik',
-    host: 'localhost',
-    port: 5432,
-    max: 10
-});
-
-pool.connect((err, db, done)=> {
-    if(err){
-        return console.log(err);
-    } 
-    else {
-        db.query('SELECT * from users', (err, table) => {
-            done();
-            if(err){
-                return console.log(err)
-            }
-            else{
-                console.log(table.rows)
-            }
-        })
-    }
-})
-
-
+app.use(express.json());
 app.use(bodyParser.json());
 
 // DB Config
