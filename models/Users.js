154fdad2366c466b6300f724a11a7607d7ae1ca0
tariktabakovic <tariktab97@gmail.com>
2020-03-08 22:24:23 -0400

    creates User Schema with name, email, password, and register date

--- models/Users.js
+++ models/Users.js
@@ -1,1 +1,21 @@
 import { Schema, model } from 'mongoose';
+
+const UserSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    register_date:{
+        type: Date, 
+        default: Date.now
+    }
+});
