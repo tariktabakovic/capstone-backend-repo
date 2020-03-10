1bb37b9fe1232b00573d6eb5442bb22a35d3bf2a
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2020-03-10 13:56:58 -0400

    creates route for getting user data

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const config = require('config');
 const jwt = require('jsonwebtoken');
+const auth = require('../../middleware/auth');
 // User Model
 const User = require('../../models/User');
 
 
 /**
  * @route   POST api/auth
- * @desc    Get all users
+ * @desc    Auth users
  * @access  Public
  */
 
@@ -47,5 +48,16 @@ router.post('/', (req, res) =>{
                 })
         })
 });
+
+/**
+ * @route   GET api/auth/user
+ * @desc    Get user data
+ * @access  Private
+ */
+router.get('/user', auth, (req,res) =>{
+    User.findById(req.user.id)
+        .select('-password')
+        .then(user => res.json(user));
+})
 
 module.exports = router;
