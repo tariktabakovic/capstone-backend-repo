90a3a8e687c4d8f61c524b907edc7a08ddea6703
tariktabakovic <tariktab97@gmail.com>
2020-03-10 13:03:23 -0400

    fix typo in impots

--- models/DailyThoughts.js
+++ models/DailyThoughts.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+// import {Schema, model} from 'mongoose';
 
 // Create Schema
 const DailyThoughtSchema = new Schema ({
-    thought: {
-        type: String,
+    name: {
+            type: String,
             required: true
         },
-        date: {
-            type: Date,
-            default: Date.now
+    date: {
+        type: Date,
+        default: Date.now
     }
 });
 
+// const DailyThought = model('dailythought', DailyThoughtSchema);
+// export default DailyThought;
 module.exports = DailyThought = mongoose.model('dailythought', DailyThoughtSchema);
