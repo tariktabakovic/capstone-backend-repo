278d2aeebd7026f98b0eeaca9f89134612bec84e
Tarik Tabakovic <tariktabakovic@Tariks-MacBook-Air.local>
2020-03-10 13:28:50 -0400

    implements token from jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -38,13 +38,22 @@ router.post('/', (req, res) =>{
                     newUser.password = hash;
                     newUser.save()
                         .then(user =>{
-                            res.json({
-                                user: {
-                                    id: user.id,
-                                    name: user.name,
-                                    email: user.email
+                            jwt.sign(
+                                {id: user.id},
+                                config.get('jwtSecret'),
+                                {expiresIn: 6600},
+                                (err, token) => {
+                                    if(err) throw err;
+                                    res.json({
+                                        token,
+                                        user: {
+                                            id: user.id,
+                                            name: user.name,
+                                            email: user.email
+                                        }
+                                    })
                                 }
-                            })
+                            )
                     })
                 })
             })
