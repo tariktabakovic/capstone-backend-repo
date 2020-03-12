29d0cc98118c1aae01dadfa6aadf6cf09bb5b907
tariktabakovic <tariktab97@gmail.com>
2020-03-12 09:23:37 -0400

    implements middleware for verifying user

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ function auth(req, res, next){
     const token = req.header('x-auth-token');
 
     // Check for token
-    if(!token) res.status(401).json({msg: 'Authorization denied; no token'});
+    if(!token) return res.status(401).json({msg: 'Authorization denied; no token'});
 
     try{
         // Verify token
